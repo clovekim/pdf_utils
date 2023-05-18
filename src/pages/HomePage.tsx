d4a3b6b668bb2647e2c8651e70ab84c7d7822113
khkim <gihun000@gmail.com>
2023-05-18 20:23:38 +0900

    initial settings

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import { Button } from 'antd';
+
+const HomePage: FC = function HomePage() {
+  return (
+    <div>
+      <Button>test</Button>
+    </div>
+  );
+};
+
+export { HomePage };
---
+++ src/pages/TestViewerPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const TestViewerPage: FC = function TestViewerPage() {
+  return <div>test</div>;
+};
+
+export { TestViewerPage };
