b93845e87a6b944f22b6b882f403e5a0e324c6d0
khkim <gihun000@gmail.com>
2023-05-19 14:15:00 +0900

    react-pdf viewer(import) 성공

--- src/components/MyPdfViewer.tsx
+++ src/components/MyPdfViewer.tsx
@@ -1,25 +1,50 @@
 import { useState } from 'react';
 
+import type { PDFDocumentProxy } from 'pdfjs-dist';
 import { Document, Page, pdfjs } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+const options = {
+  cMapUrl: 'cmaps/',
+  standardFontDataUrl: 'standard_fonts/',
+};
+
+type PDFFile = string | File | null;
 
 function MyPdfViewer() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [file, setFile] = useState<null | PDFFile>(null);
+  const [numPages, setNumPages] = useState<number>();
 
-  function onDocumentLoadSuccess({ numPages }: any) {
-    setNumPages(numPages);
+  function onFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target;
+
+    if (files && files[0]) {
+      setFile(files[0] || null);
+    }
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy) {
+    setNumPages(nextNumPages);
   }
 
   return (
-    <div>
-      <Document file="src/assets/pdfs/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+    <div className="Example">
+      <header>
+        <h1>react-pdf sample page</h1>
+      </header>
+      <div className="Example__container">
+        <div className="Example__container__load">
+          <label htmlFor="file">Load from file:</label>{' '}
+          <input onChange={onFileChange} type="file" />
+        </div>
+        <div className="Example__container__document">
+          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
+        </div>
+      </div>
     </div>
   );
 }
